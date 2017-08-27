59fea66fd743d3c9aec50be8796ecc755ec93e9a
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-27 16:06:38 +0200

    auto reconnect

--- Dashboard/mqtt2mqtt.py
+++ Dashboard/mqtt2mqtt.py
@@ -2,36 +2,67 @@ import time
 import ConfigParser
 import paho.mqtt.client as mqtt
 
-config = ConfigParser.ConfigParser()
+config           = ConfigParser.ConfigParser()
 config.read("config.ini")
-intern = mqtt.Client("Mqtt2Mqtt", clean_session=True)
-extern = mqtt.Client("Mqtt2Mqtt", clean_session=True)
+intern           = mqtt.Client("Mqtt2Mqtt", clean_session=True)
+extern           = mqtt.Client("Mqtt2Mqtt", clean_session=True)
+extern_connected = False
+intern_connected = False
 
-def _on_connect(client, userdata, rc, msg):
+
+def _on_intern_connect(client, userdata, rc, msg):
     #print ("Connected %s with result code %s" % (client, rc))
+    intern_connected = True
     intern.subscribe("#")
 
 
-def _on_disconnect(client, userdata, msg):
-    pass
+def _on_intern_disconnect(client, userdata, msg):
+    intern_connected = False
     #print ("Disconnect %s" % client)
 
 
-def _on_message(client, userdata, msg):
+def _on_intern_message(client, userdata, msg):
     #print("Mq Received on channel %s -> %s" % (msg.topic, msg.payload))
     extern.publish(msg.topic, msg.payload)
 
-intern.on_connect    = _on_connect
-intern.on_message    = _on_message
-intern.on_disconnect = _on_disconnect
-intern.connect(config.get("intern", "host"), int(config.get("intern", "port")))
-intern.loop_start()
+
+def _on_extern_connect(client, userdata, rc, msg):
+    #print ("Connected %s with result code %s" % (client, rc))
+    #intern.subscribe("#")
+    extern_connected = True
+
+
+def _on_extern_disconnect(client, userdata, msg):
+    #print ("Disconnect %s" % client)
+    extern_connected = False
+
+
+def _on_extern_message(client, userdata, msg):
+    pass
+    #print("Mq Received on channel %s -> %s" % (msg.topic, msg.payload))
+    #extern.publish(msg.topic, msg.payload)
+
+intern.on_connect    = _on_intern_connect
+intern.on_message    = _on_intern_message
+intern.on_disconnect = _on_intern_disconnect
 
 extern.username_pw_set(config.get("extern", "user"), config.get("extern", "passwd"))
-extern.on_connect    = _on_connect
-extern.on_disconnect = _on_disconnect
-extern.connect(config.get("extern", "host"), int(config.get("extern", "port")))
-extern.loop_start()
+extern.on_connect    = _on_extern_connect
+extern.on_message    = _on_extern_message
+extern.on_disconnect = _on_extern_disconnect
+
 
 while True:
-    time.sleep(100)
+
+    try:
+        if not intern_connected:
+            intern.connect(config.get("intern", "host"), int(config.get("intern", "port")))
+            intern.loop_start()
+
+        if not extern_connected:
+            extern.connect(config.get("extern", "host"), int(config.get("extern", "port")))
+            extern.loop_start()
+    except Exception as e :
+        print e
+
+    time.sleep(10)
